b9da99396878b68e07c93577e8bf5f34533a2e3c
Javier López Úbeda <jlopezcur@gmail.com>
2024-04-17 16:03:53 +0200

    test: added test for resolveCollisions

--- src/dynamics/solidGravityDynamic/resolveCollisions.test.ts
+++ src/dynamics/solidGravityDynamic/resolveCollisions.test.ts
@@ -11,13 +11,28 @@ describe('dynamics', () => {
           'branch preference',
           [
             { x: 0, y: 4, w: 2, h: 2, i: '1' },
-            { x: 0, y: 3, w: 2, h: 2, i: '2', priority: 1 },
+            { x: 0, y: 3, w: 2, h: 2, i: '2', priority: Infinity },
             { x: 0, y: 2, w: 2, h: 2, i: '3' },
           ],
           [
-            { x: 0, y: 7, w: 2, h: 2, i: '1' },
-            { x: 0, y: 3, w: 2, h: 2, i: '2', priority: 1 },
-            { x: 0, y: 5, w: 2, h: 2, i: '3' },
+            { x: 0, y: 7, w: 2, h: 2, i: '1', priority: 3 },
+            { x: 0, y: 3, w: 2, h: 2, i: '2', priority: Infinity },
+            { x: 0, y: 5, w: 2, h: 2, i: '3', priority: 3 },
+          ],
+        ],
+        [
+          'branch preference each iter with gap',
+          [
+            { x: 1, y: 0, w: 2, h: 2, i: '1', priority: Infinity },
+            { x: 2, y: 0, w: 2, h: 2, i: '2' },
+            { x: 4, y: 0, w: 2, h: 3, i: '3' },
+            { x: 2, y: 3, w: 4, h: 2, i: '4' },
+          ],
+          [
+            { x: 1, y: 0, w: 2, h: 2, i: '1', priority: Infinity },
+            { x: 2, y: 2, w: 2, h: 2, i: '2', priority: 4 },
+            { x: 4, y: 0, w: 2, h: 3, i: '3' },
+            { x: 2, y: 4, w: 4, h: 2, i: '4', priority: 3 },
           ],
         ],
       ];
