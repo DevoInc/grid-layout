f2b4fc3544da2b0407e2f7913f929819a236a981
Javier López Úbeda <jlopezcur@gmail.com>
2024-12-20 13:50:52 +0100

    build: migrate to tsup from vite

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  minify: 'terser',
+  format: ['esm', 'cjs'],
+  metafile: true,
+  treeshake: true,
+});
